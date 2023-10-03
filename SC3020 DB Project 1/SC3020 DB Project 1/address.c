c2dc37cc3a16ccd9351b565ccc339e657dc13f90
Nagammai <125187878+keshavrama@users.noreply.github.com>
2023-10-03 13:13:51 +0800

    Add files via upload

---
+++ SC3020 DB Project 1/SC3020 DB Project 1/address.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// To find the address of the block where the record is stored. 
+
+struct Address {
+    int blockID; 
+    int offset;  
+};
+
+struct Address createAddress(int blockID, int offset) {
+    struct Address address;
+    address.blockID = blockID;
+    address.offset = offset;
+    return address;
+}
+
+int getBlockId(struct Address address) {
+    return address.blockID;
+}
+
+int getOffset(struct Address address) {
+    return address.offset;
+}
+
+void toString(struct Address address) {
+    printf("blk %d offset %d\n", address.blockID, address.offset);
+}
+
+/*int main() {
+    struct Address address = createAddress(1, 42);
+    printf("Block ID: %d\n", getBlockId(address));
+    printf("Offset: %d\n", getOffset(address));
+    toString(address);
+    return 0;
+}
+*/
