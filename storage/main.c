ec4bfe38d4e9b7ab9170cef85c71eadf021917d5
capnhayfay <singh.dhruv2000@gmail.com>
2023-10-06 22:24:43 +0800

    message

--- storage/main.c
+++ storage/main.c
@@ -23,7 +23,60 @@ int main() {
     }
     //visualizeDisk(disk);
 
+<<<<<<< HEAD
+    char line[255];
+    fgets(line, sizeof(line), file); // Skip header line
+
+    int currentBlockId = 0; 
+    struct Block* currentBlock = getBlock(disk, currentBlockId);
+
+     int lineCounter = 0;  // To track the line number
+
+    while (fgets(line, sizeof(line), file)) {
+        lineCounter++;
+
+        // Debug
+        printf("Line %d: %s", lineCounter, line);
+
+        // Parse the line to create a record
+        struct Record record;
+
+        // Parse the date string and convert it to an integer
+        int day, month, year;
+        sscanf(line, "%d/%d/%d %d %d %f %f %f %d %d %s", &day, &month, &year, &record.TEAM_ID_home,
+               &record.PTS_home, &record.FG_PCT_home, &record.FT_PCT_home,
+               &record.FG3_PCT_home, &record.AST_home, &record.REB_home,
+               &record.HOME_TEAM_WINS);
+
+        // Convert the date to a single integer (e.g., 20220425)
+        record.GAME_DATE_EST = (year * 10000) + (month * 100) + day;
+
+        // Step 3: Populate the Disk
+        if (currentBlock->curRecords >= (int)(MAX_RECORDS)) {  // Assuming average of 7 records per block
+            currentBlockId++;
+            currentBlock = getBlock(disk, currentBlockId);
+        }
+
+        if (currentBlock->curRecords >= (int)(MAX_RECORDS)) {
+            printf("Block is full. This shouldn't happen.\n");
+            return 1;
+        }
+
+
+        writeRecordToStorage(disk, record);
+        
+        // Debug
+        //printf("Record No.: %d, Stored in Block: %d\n", disk->numOfRecords, currentBlockId);
+
+       
+    }
+    
+    fclose(file);
+    printf("Data loaded into disk.\n");
+
+=======
     //EXPERIMENT 1
+>>>>>>> main
     printf("\n----------------------EXPERIMENT 1-----------------------\n");
     printf("Total Number of Records Stored: %d\n", disk->numOfRecords);
     printf("Size of Each Record: %zu Bytes\n", sizeof(struct Record));
